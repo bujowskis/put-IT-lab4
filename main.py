cc10f2eade8b67efc4d8231403f686aef33291c6
bujowskis <s.bujowski@gmail.com>
2021-12-07 13:45:45 +0100

    computing rank-grams using characters seems to work

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ def computeCharEntropy(filepath, rank: int = 0):
     :param filepath: path to the text file to be analyzed
     :param rank: rank of the entropy, i.e. on how many other characters this one depends
     """
-    enmat = list()  # entropy matrix
+    count = dict()  # counts of occurrence of all different rank-grams
     with open(filepath, 'r') as file:
-        ...
+        contents = file.readline()  # as everything is in one row
+
+        # "- rank" due to first chars being cut out (not enough prior chars)
+        for char_idx in reversed(range(len(contents) - rank)):
+            rank_gram = ""
+            for shift in reversed(range(rank+1)):
+                rank_gram += contents[char_idx - shift]
+            if rank_gram in count:
+                count[rank_gram] = count[rank_gram] + 1
+            else:
+                count[rank_gram] = 1
+    print(count)
 
 
 def computeWordEntropy(filepath, rank: int = 0):
@@ -21,3 +32,8 @@ def computeWordEntropy(filepath, rank: int = 0):
     """
     ...
 
+
+# main program TODO - make a "centralization function" reading the text file only once
+computeCharEntropy("text-files/norm_wiki_en.txt", 0)
+computeCharEntropy("text-files/norm_wiki_en.txt", 1)
+computeCharEntropy("text-files/norm_wiki_en.txt", 2)
